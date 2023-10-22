9b7526fccabb8e7c05c7e319135e1aea8b822eab
joicelyne <129493160+joicelyne@users.noreply.github.com>
2023-10-22 14:16:29 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,71 @@
+<footer class="footer-distributed">
+    <div class="container-footer">
+    <div class="line-container">
+        <div class="line-foot"></div>
+        <div class="logo-container">
+            <img src="images/mainlogo.png" alt="Logo" class="logo-foot">
+        </div>
+        <div class="line-foot"></div>
+    </div>
+</div>
+    
+    <div class="footer-left">
+
+        <h3>Golden<span>PR</span></h3>
+
+        <p class="footer-links">
+            <a href="#" class="link-1">Home</a>
+
+            <a href="menu.php">Menu</a>
+
+            <a href="orders.php">Orders</a>
+
+            <a href="about.php">About</a>
+
+            <a href="contact.php">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Golden PR @ 2023</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+        </div>
+
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>+1.555.555.5555</p>
+        </div>
+
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p>support@company.com</p>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+
+        <p class="footer-company-about">
+            <span>Restaurants Hours</span>
+            <span>Monday - Tueday : 9AM - 10PM</span>
+            <span>Wednesday : 9AM - 2AM</span>
+            <span>Thursday - Friday : 9AM - 10PM</span>
+            <span>Sunday : Closed</span>
+        </p>
+
+        <div class="footer-icons">
+
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-github"></i></a>
+
+        </div>
+
+    </div>
+
+</footer>
